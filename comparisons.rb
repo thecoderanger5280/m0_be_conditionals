1080e4d36bcd29dd66d2feb8b1b73b82d33b7570
Ryan Loughry <thecoderanger5280@icloud.com>
2022-07-08 20:55:33 -0600

    Complete the comparisons exersize

--- comparisons.rb
+++ comparisons.rb
@@ -23,25 +23,25 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
-
+puts numberTeachers < numberStudents
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
-
+puts numberTeachers == string_teachers
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
-
+puts numberTeachers != numberStudents
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
-
+puts numberStudents >= 20
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
-
+puts numberStudents >= 21
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
-
+puts numberStudents <= 20
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
-
+puts numberStudents <= 21
 
 #-------------------
 # PART 2: Articulating what you are doing
@@ -54,21 +54,21 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
-
+#this will check if 4 is less than 9 so it will output true because 4 is less than 9
 books = 3
 puts 4 < books
 # YOU DO: Explain.
-
+#this sets the variable "books" to 3 and then checks if 4 is less than 3 so it will output false
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
-
+#this sets variable "friends" to 6 and variable "siblings" to 2 and checks if 6 is greater than 2 so it will output true
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
-
+#this sets variables "attendees" to 9 and "meals" to 8 and checks if 9 is not eaqual to 8 so it will output true
 
 #-------------------
 # PART 3: Logical Operators
@@ -90,15 +90,15 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
-
+loves_to_play && age < 2
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER:
